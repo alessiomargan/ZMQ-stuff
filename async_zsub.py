813d2c5672b0d1702f9933c9eccb4245c503bc84
Alessio Margan <alessio.margan@iit.it>
2023-10-18 17:31:03 +0200

    wip async

--- async_zsub.py
+++ async_zsub.py
@@ -7,9 +7,13 @@ import ecat_pdo_pb2
 import asyncio
 import zmq
 import zmq.asyncio
+import argparse
 import pprint
 import json
 from protobuf_to_dict import protobuf_to_dict
+from contextlib import suppress
+
+ctx = zmq.asyncio.Context()
 
 # Define a global variable to store the last received data
 last_received_data = None,None
@@ -18,28 +22,34 @@ def process_data(msg_id, data):
     rx_pdo = ecat_pdo_pb2.Ec_slave_pdo()
     rx_pdo.ParseFromString(data)
     pb_dict = protobuf_to_dict(rx_pdo)
-    last_received_data = msg_id,pb_dict
     return msg_id,pb_dict
 
-#from utils import get_address
-def get_address():
-    protocol = "tcp"
-    address = "127.0.0.1"
-    port = 9601
-    return f"{protocol}://{address}:{port}"
-
-async def zsub():
-    subscriber = zmq.asyncio.Context().socket(zmq.SUB)
-    subscriber.connect(get_address())
+async def zsub(args):
+    global last_recv_data
+    subscriber = ctx.socket(zmq.SUB)
+    uri = f"{args.protocol}://{args.address}:{args.port}"
+    print("connect to", uri)
+    subscriber.connect(uri)
     subscriber.subscribe(b"")
-
-    while True:
-        #data = await subscriber.recv_json()
-        msg_id, data = await subscriber.recv_multipart()
-        # Process data as needed
-        msg_id, pb_dict = process_data(msg_id, data)
-        pprint.pprint((msg_id, pb_dict))
-
+    with suppress(asyncio.CancelledError):
+        while True:
+            #data = await subscriber.recv_json()
+            msg_id, data = await subscriber.recv_multipart()
+            # store the last received data
+            last_recv_data = process_data(msg_id, data)
+            pprint.pprint(last_recv_data)
+    subscriber.close()
         
 if __name__ == '__main__':
-    asyncio.run(zsub())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-P', '--protocol', type=str,
+                        choices=['tcp', 'udp', 'inproc'], default='tcp')
+    parser.add_argument('-a', '--address',  type=str, default='127.0.0.1')
+    parser.add_argument('-p', '--port',     type=int)
+    args = parser.parse_args()
+    print(args)
+    try:
+        asyncio.run(zsub(args))
+    except KeyboadInterrupt:
+        print('Exiting ...')
+        ctx.term()
